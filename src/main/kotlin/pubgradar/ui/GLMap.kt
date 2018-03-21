d5f6a6aa2c326e8a65a4fd10d0bde2a7016d2fa4
ReddeR1337 <11780299+ReddeR1337@users.noreply.github.com>
2018-03-22 02:01:38 +0300

    upd

--- src/main/kotlin/pubgradar/ui/GLMap.kt
+++ src/main/kotlin/pubgradar/ui/GLMap.kt
@@ -171,30 +171,36 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
     private val aimStartTime = HashMap<NetworkGUID, Long>()
     private val attackLineStartTime = LinkedList<Triple<NetworkGUID, NetworkGUID, Long>>()
     private val pinLocation = Vector2()
-    // Menu Settings
-    //////////////////////////////
-    private var filterWeapon = -1
-    private var filterAttach = -1
-    private var filterLvl2 = -1
-    private var filterScope = -1
-    private var filterHeals = -1
-    private var filterAmmo = 1
-    private var filterThrow = 1
-    private var drawcompass = -1
-    private var drawmenu = 1
-    private var toggleView = 1
-    private var drawDaMap = 1
+
+
+    // Please change your pre-build settings in Settings.kt
+    // You can change them in Settings.json after you run the game once too.
+    private var filterWeapon = jsettings.filterWeapon
+    private var filterAttach = jsettings.filterAttach
+    private var filterLvl2 = jsettings.filterLvl2
+    private var filterScope = jsettings.filterScope
+    private var filterHeals = jsettings.filterHeals
+    private var filterAmmo = jsettings.filterAmmo
+    private var filterThrow = jsettings.filterThrow
+    private var drawcompass = jsettings.drawcompass
+    private var drawmenu = jsettings.drawmenu
+    private var toggleView = jsettings.toggleView
+    private var drawDaMap = jsettings.drawDaMap
+
+    // Please change your pre-build settings in Settings.kt
+    // You can change them in Settings.json after you run the game once too.
+
     // private var toggleVehicles = -1
     //  private var toggleVNames = -1
-    private var drawMinimap = -1
-    private var nameToggles = 4
-    private var VehicleInfoToggles = 1
-    private var ZoomToggles = 1
-    ///////////////////////////
+
+    private var nameToggles = jsettings.nameToggles
+    private var VehicleInfoToggles = jsettings.VehicleInfoToggles
+    private var ZoomToggles = jsettings.ZoomToggles
     private var scopesToFilter = arrayListOf("")
     private var weaponsToFilter = arrayListOf("")
     private var attachToFilter = arrayListOf("")
     private var level2Filter = arrayListOf("")
+    private var level1Filter = arrayListOf("")
     private var healsToFilter = arrayListOf("")
     private var ammoToFilter = arrayListOf("")
     private var throwToFilter = arrayListOf("")
@@ -203,6 +209,37 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
     private var prevScreenY = -1f
     private var screenOffsetX = 0f
     private var screenOffsetY = 0f
+
+
+     val miniMapWindowWidth=jsettings.miniMapWindowWidth
+     val miniMapRadius=jsettings.miniMapRadius
+     val playerRadius=jsettings.playerRadius
+     val healthBarWidth=jsettings.healthBarWidth
+     val healthBarHeight=jsettings.healthBarHeight
+     val directionRadius=jsettings.directionRadius
+     val fov=jsettings.fov
+     val aimLineWidth=jsettings.aimLineWidth
+     val aimLineRange=jsettings.aimLineRange
+     val aimCircleRadius=jsettings.aimCircleRadius
+     val aimTimeThreshold=jsettings.aimTimeThreshold
+     val attackLineDuration=jsettings.attackLineDuration
+     val attackMeLineDuration=jsettings.attackMeLineDuration
+     val firingLineDuration = jsettings.firingLineDuration
+     val firingLineLength = jsettings.firingLineLength
+     val itemZoomThreshold= jsettings.itemZoomThreshold
+     val airDropTextScale = jsettings.airDropTextScale
+     val itemScale = jsettings.itemScale
+     val staticItemScale = jsettings.staticItemScale
+     val mapMarkerScale = jsettings.mapMarkerScale
+     val airDropScale = jsettings.airDropScale
+     val vehicleScale = jsettings.vehicleScale
+     val planeScale = jsettings.planeScale
+     val grenadeScale = jsettings.grenadeScale
+     val corpseScale = jsettings.corpseScale
+     val redzoneBombScale = jsettings.redzoneBombScale
+
+    // Please change your pre-build settings in Settings.kt
+    // You can change them in Settings.json after you run the game once too.
 
 
     private fun windowToMap(x: Float, y: Float) =
@@ -306,6 +343,18 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
                     camera.zoom = 1 / 8f
                 }
             }
+
+         // Level 1 and 2 item filters
+            Input.Keys.valueOf(jsettings.filterLvl2_Key) -> {
+                if (filterLvl2 < 3) {
+                    filterLvl2 += 1
+                }
+                if (filterLvl2 == 3) {
+                    filterLvl2 = 0
+                }
+            }
+
+        // Please Change Your Settings in Util/Settings.kt
         // Other Filter Keybinds
             Input.Keys.valueOf(jsettings.drawcompass_Key) -> drawcompass = drawcompass * -1
 
@@ -321,7 +370,6 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
 
         // Icon Filter Keybinds
             Input.Keys.valueOf(jsettings.filterWeapon_Key) -> filterWeapon = filterWeapon * -1
-            Input.Keys.valueOf(jsettings.filterLvl2_Key) -> filterLvl2 = filterLvl2 * -1
             Input.Keys.valueOf(jsettings.filterHeals_Key) -> filterHeals = filterHeals * -1
             Input.Keys.valueOf(jsettings.filterThrow_Key) -> filterThrow = filterThrow * -1
             Input.Keys.valueOf(jsettings.filterAttach_Key) -> filterAttach = filterAttach * -1
@@ -332,6 +380,7 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
             Input.Keys.valueOf(jsettings.camera_zoom_Minus_Key) -> mapCamera.zoom = mapCamera.zoom + 0.00525f
             Input.Keys.valueOf(jsettings.camera_zoom_Plus_Key) -> mapCamera.zoom = mapCamera.zoom - 0.00525f
 
+        // Please Change Your Settings in Util/Settings.kt
         }
         return false
     }
@@ -670,7 +719,7 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
             else
                 espFontShadow.draw(spriteBatch, "ATTACH", 40f, windowHeight - 42f)
 
-            if (filterLvl2 != 1)
+            if (filterLvl2 != 0)
                 espFont.draw(spriteBatch, "EQUIP", 100f, windowHeight - 25f)
             else
                 espFontShadow.draw(spriteBatch, "EQUIP", 100f, windowHeight - 25f)
@@ -742,9 +791,13 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
                 else
                     menuFontOFF.draw(spriteBatch, "Disabled", 187f, windowHeight / 2 + 103f)
 
-                if (filterLvl2 != 1)
-                    menuFontOn.draw(spriteBatch, "Enabled", 187f, windowHeight / 2 + 85f)
-                else
+                if (filterLvl2 == 1)
+                    menuFontOn.draw(spriteBatch, "Level 1", 187f, windowHeight / 2 + 85f)
+
+                if (filterLvl2 == 2)
+                    menuFontOn.draw(spriteBatch, "Level 2", 187f, windowHeight / 2 + 85f)
+
+                if (filterLvl2 == 0)
                     menuFontOFF.draw(spriteBatch, "Disabled", 187f, windowHeight / 2 + 85f)
 
                 if (filterHeals != 1)
@@ -1385,31 +1438,30 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
                     "Item_Weapon_Molotov_C")
         }
 
-        level2Filter = if (filterLvl2 != 1) {
-            arrayListOf("")
-        } else {
+        level2Filter = if (filterLvl2 < 2) {
             arrayListOf(
+                 // "Item_Armor_C_01_Lv3_C",
+                 // "Item_Head_G_01_Lv3_C",
+                //  "Item_Back_C_02_Lv3_C",
+                //  "Item_Back_C_01_Lv3_C",
                     "Item_Armor_D_01_Lv2_C",
-                    "Item_Armor_C_01_Lv3_C",
-                    "Item_Head_G_01_Lv3_C",
                     "Item_Head_F_02_Lv2_C",
                     "Item_Head_F_01_Lv2_C",
-                    "Item_Back_C_02_Lv3_C",
-                    "Item_Back_C_01_Lv3_C",
                     "Item_Back_F_01_Lv2_C",
-                    "Item_Back_F_02_Lv2_C",
+                    "Item_Back_F_02_Lv2_C")
+        }else { arrayListOf("") }
+
+        level1Filter = if (filterLvl2 < 1) {
+            arrayListOf(
                     "Item_Back_E_01_Lv1_C",
                     "Item_Armor_E_01_Lv1_C",
                     "Item_Head_E_01_Lv1_C",
                     "Item_Back_E_02_Lv1_C",
                     "Item_Head_E_02_Lv1_C")
+        } else {
+            arrayListOf("")
         }
 
-        val Crateitems = arrayListOf("Item_Weapon_AUG",
-                "Item_Weapon_M24",
-                "Item_Weapon_M249",
-                "Item_Weapon_Groza",
-                "Item_Weapon_AWM")
 
         val sorted = ArrayList(droppedItemLocation.values)
         sorted.sortBy {
@@ -1423,7 +1475,7 @@ class GLMap(private val jsettings: Settings.jsonsettings) : InputAdapter(), Appl
             val scale = if (it._3) itemScale else staticItemScale
 
             if ((items !in weaponsToFilter && items !in scopesToFilter && items !in attachToFilter && items !in level2Filter
-                            && items !in ammoToFilter && items !in healsToFilter) && items !in throwToFilter) {
+                            && items !in level1Filter && items !in ammoToFilter && items !in healsToFilter) && items !in throwToFilter) {
                 if (items in crateIcons) {
 
                     val adt = crateIcons[items]!!
--- src/main/kotlin/pubgradar/util/Settings.kt
+++ src/main/kotlin/pubgradar/util/Settings.kt
@@ -1,14 +1,113 @@
 package main.util
 
+
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import java.io.File
-
 class Settings {
 
     data class jsonsettings(
+            ////
+            //// Style Settings
+            ////
+            val miniMapWindowWidth: Float = 400f,
+            val miniMapRadius: Float = 500 * 100f,
+            val playerRadius: Float = 4000f,
+            val healthBarWidth: Float = 15000f,
+            val healthBarHeight: Float = 2000f,
+            val directionRadius: Float = 16000f,
+
+            val fov: Float = 90f,
+
+            val aimLineWidth: Float = 1000f,
+            val aimLineRange: Float = 50000f,
+            val aimCircleRadius: Float = 200f,
+            val firingLineLength: Float = 20000f,
+            val itemZoomThreshold: Float = 0.06f,
+            val airDropTextScale: Float = 1000f,
+            val itemScale: Float = 16f,
+            val staticItemScale: Float = 200f,
+            val mapMarkerScale: Float = 150f,
+            val airDropScale: Float = 250f,
+            val vehicleScale: Float = 25f,
+            val planeScale: Float = 350f,
+            val grenadeScale: Float = 15f,
+            val corpseScale: Float = 30f,
+            val redzoneBombScale: Float = 30f,
+            val aimTimeThreshold: Int = 1000,
+            val attackLineDuration: Int = 1000,
+            val attackMeLineDuration: Int = 10000,
+            val firingLineDuration: Int = 500,
+
+
+
+            ///
+            /// Information Toggles
+            /// Default Item Information Toggles
+            // -1 Disabled
+            // 1  Enabled
+            var filterWeapon: Int = -1,
+            var filterAttach: Int = -1,
+            var filterScope: Int = -1,
+            var filterHeals: Int = -1,
+            var filterAmmo: Int = 1,
+            var filterThrow: Int = 1,
+
+            // Draw Compass
+            var drawcompass: Int = -1,
+
+            // Draw Menu
+            var drawmenu: Int = 1,
+
+            // Toggle View Line
+            var toggleView: Int = 1,
+
+            // Toggle Mini-Map
+            var drawDaMap: Int = 1,
+            // private var toggleVehicles = -1
+            //  private var toggleVNames = -1
+
+            // Player Info Toggles 1-4
+            var nameToggles: Int = 4,
+
+            // Filter Equipment 1-2
+            var filterLvl2: Int = 1,
+
+            // Vehicle Information Toggles 1-2
+            var VehicleInfoToggles: Int = 1,
+
+            // Zoom Toggles 1-3
+            var ZoomToggles: Int = 1,
+
+
+            //
+            //  Key settings
+            //  Scarjit: Im using strings here for user comfort
+            //
+            val nameToogle_Key: String = Input.Keys.toString(Input.Keys.F1),
+            val VehicleInfoToggles_Key: String = Input.Keys.toString(Input.Keys.F5),
+            val ZoomToggles_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_8),
+
+            val drawcompass_Key: String = Input.Keys.toString(Input.Keys.F2),
+            val toggleView_Key: String = Input.Keys.toString(Input.Keys.F4),
+            val drawDaMap_Key: String = Input.Keys.toString(Input.Keys.F3),
+            val drawmenu_Key: String = Input.Keys.toString(Input.Keys.F12),
+
+            val filterWeapon_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_1),
+            val filterLvl2_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_2),
+            val filterHeals_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_3),
+
+            val filterThrow_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_4),
+            val filterAttach_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_5),
+            val filterScope_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_6),
+            val filterAmmo_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_7),
+
+            val camera_zoom_Minus_Key: String = Input.Keys.toString(Input.Keys.MINUS),
+            val camera_zoom_Plus_Key: String = Input.Keys.toString(Input.Keys.PLUS),
+
+
 
             //
             //  Font settings
@@ -17,11 +116,11 @@ class Settings {
             //AGENCYFB
             val hubFont_size: Int = 30,
             val hubFont_color: Color = Color.WHITE,
-            val hubFontShadow_color: Color = Color(1f,1f,1f,0.4f),
+            val hubFontShadow_color: Color = Color(1f, 1f, 1f, 0.4f),
 
             val espFont_size: Int = 16,
             val espFont_color: Color = Color.WHITE,
-            val espFontShadow_color: Color = Color(1f,1f,1f,0.2f),
+            val espFontShadow_color: Color = Color(1f, 1f, 1f, 0.2f),
 
             //NUMBER
             val largeFont_size: Int = 38,
@@ -69,35 +168,7 @@ class Settings {
             val hpgreen_color: Color = Color.GREEN,
 
             val hpred_size: Int = 10,
-            val hpred_color: Color = Color.RED,
-
-
-            //
-            //  Key settings
-            //  Scarjit: Im using strings here for user comfort
-            //
-            val nameToogle_Key: String = Input.Keys.toString(Input.Keys.F1),
-            val VehicleInfoToggles_Key: String = Input.Keys.toString(Input.Keys.F5),
-            val ZoomToggles_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_8),
-
-            val drawcompass_Key: String = Input.Keys.toString(Input.Keys.F2),
-            val toggleView_Key: String = Input.Keys.toString(Input.Keys.F4),
-            val drawDaMap_Key: String = Input.Keys.toString(Input.Keys.F3),
-            val drawmenu_Key: String = Input.Keys.toString(Input.Keys.F12),
-
-            val filterWeapon_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_1),
-            val filterLvl2_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_2),
-            val filterHeals_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_3),
-
-            val filterThrow_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_4),
-            val filterAttach_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_5),
-            val filterScope_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_6),
-
-            val filterAmmo_Key: String = Input.Keys.toString(Input.Keys.NUMPAD_7),
-
-            val camera_zoom_Minus_Key: String = Input.Keys.toString(Input.Keys.MINUS),
-            val camera_zoom_Plus_Key: String = Input.Keys.toString(Input.Keys.PLUS)
-
+            val hpred_color: Color = Color.RED
 
 
     )
@@ -117,7 +188,7 @@ class Settings {
         val set = adapter.fromJson(f.readText())
         if (set != null) {
             return set
-        }else{
+        } else {
             throw NullPointerException()
         }
     }
