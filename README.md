56fcb32b25cf0f3492b68d804eefbc2108a47c11
ReddeR1337 <11780299+ReddeR1337@users.noreply.github.com>
2018-03-23 21:19:55 +0300

    Very Meme Radar http://VMRadar.net

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 VMRadar Website located here -> https://vmradar.net/
+
 Original GitHub located here -> https://github.com/Jerry1211/RadarProject
--- src/main/kotlin/pubgradar/util/Notify.kt
+++
@@ -1,8 +0,0 @@
-package pubgradar.util
-
-import javax.swing.JOptionPane
-
-fun notify(message : String)
-{
-   JOptionPane.showMessageDialog(null , message)
-}
